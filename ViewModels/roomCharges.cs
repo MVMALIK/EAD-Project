53382fccdcc56049ab698ce614a0b43d1558282d
mv <bcsf14m054@gmail.com>
2017-07-10 03:01:50 +0500

    Final Version

---
+++ ViewModels/roomCharges.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication3.Models;
+
+namespace WebApplication3.ViewModels
+{
+    public class roomCharges
+    {
+        public room room { get; set; }
+        public category category { get; set; }
+    }
+    public class room_categories
+    {
+        public List<room> roomData { get; set; }
+        public List<category> categoryData { get; set; }
+        public IQueryable<roomCharges> romChar { get; set; }
+
+    }
+}
